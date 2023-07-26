84d9df9eafb2c7d14c4d5645934b3e425fdfdf11
Prosper <pblinkprosper01@gmail.com>
2023-07-26 09:22:48 +0000

    Add new shell folder

---
+++ new_shell/builtin_cmd.c
@@ -0,0 +1,49 @@
+#include "shell.h"
+
+/**
+ * exec_line - finds builtins and commands
+ *
+ * @datash: data relevant (args)
+ * Return: 1 on success.
+ */
+int exec_line(data_shell *datash)
+{
+	int (*builtin)(data_shell *datash);
+
+	if (datash->args[0] == NULL)
+		return (1);
+
+	builtin = get_builtin(datash->args[0]);
+
+	if (builtin != NULL)
+		return (builtin(datash));
+
+	return (cmd_exec(datash));
+}
+
+/**
+ * get_builtin - builtin that pais the command in the arg
+ * @cmd: command
+ * Return: function pointer of the builtin command
+ */
+int (*get_builtin(char *cmd))(data_shell *)
+{
+	builtin_t builtin[] = {
+		{ "env", _env },
+		{ "exit", exit_shell },
+		{ "setenv", _setenv },
+		{ "unsetenv", _unsetenv },
+		{ "cd", cd_shell },
+		{ "help", get_help },
+		{ NULL, NULL }
+	};
+	int i;
+
+	for (i = 0; builtin[i].name; i++)
+	{
+		if (_strcmp(builtin[i].name, cmd) == 0)
+			break;
+	}
+
+	return (builtin[i].f);
+}
